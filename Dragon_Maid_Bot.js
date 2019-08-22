a8dc87bae08d931a192cf025c290378c7ee0b175
Marcelo Valencia Jr <mvale080@fiu.edu>
2019-08-22 13:33:32 -0400

    Changed Properties file to a config.json. gitignore updated to reflect this change. Added '!punch' command and certain safeguards to using it against the owner and the bot

--- Dragon_Maid_Bot.js
+++ Dragon_Maid_Bot.js
@@ -1,84 +1,97 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-var fs = require('fs')
-var text = fs.readFileSync('./Properties')
-var bot_secret_token = text.slice(':')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
 
 client.on('ready', () => {
-    console.log("Connected as " + client.user.tag)
+    console.log("Authur is: " + config.owner);
+    console.log("Connected as " + client.user.tag);
     // List servers the bot is connected to
-    console.log("Servers:")
+    console.log("Servers:");
     client.guilds.forEach((guild) => {
-        console.log(" - " + guild.name)
+        console.log(" - " + guild.name);
 
         // List all channels
         guild.channels.forEach((channel) => {
-            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
-        })
-    })
+            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`);
+        });
+    });
     // Set bot status to: "Playing with JavaScript"
-    client.user.setActivity("Master", {type: "LISTENING"})
+    client.user.setActivity("Master", {type: "LISTENING"});
 
     // Alternatively, you can set the activity to any of the following:
     // PLAYING, STREAMING, LISTENING, WATCHING
     // For example:
     // client.user.setActivity("TV", {type: "WATCHING"})
-})
+});
 
 client.on('message', (receivedMessage) => {
     if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
-        return
+        return;
     }
     
-    if (receivedMessage.content.startsWith("!")) {
-        processCommand(receivedMessage)
+    if (receivedMessage.content.startsWith(config.prefix)) {
+        processCommand(receivedMessage);
     }
-})
+});
 
 function processCommand(receivedMessage) {
-    let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
-    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
-    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
-    let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
+    let fullCommand = receivedMessage.content.substr(1); // Remove the leading exclamation mark
+    let splitCommand = fullCommand.split(" "); // Split the message up in to pieces for each space
+    let primaryCommand = splitCommand[0]; // The first word directly after the exclamation is the command
+    let arguments = splitCommand.slice(1); // All other words are arguments/parameters/options for the command
 
-    console.log("Command received: " + primaryCommand)
-    console.log("Arguments: " + arguments) // There may not be any arguments
+    console.log("Command received: " + primaryCommand);
+    console.log("Arguments: " + arguments); // There may not be any arguments
 
     if (primaryCommand == "help") {
-        helpCommand(arguments, receivedMessage)
+        helpCommand(arguments, receivedMessage);
     } else if (primaryCommand == "multiply") {
-        multiplyCommand(arguments, receivedMessage)
+        multiplyCommand(arguments, receivedMessage);
+    } else if (primaryCommand == "punch") {
+        punchCommand(arguments, receivedMessage);
     } else {
-        receivedMessage.channel.send("I don't understand the command. Try `!help` or `!multiply`")
+        receivedMessage.channel.send("I don't understand the command. Try `!help` or `!multiply`");
     }
 }
 
 function helpCommand(arguments, receivedMessage) {
     if (arguments.length > 0) {
-        receivedMessage.channel.send("It looks like you might need help with " + arguments)
+        receivedMessage.channel.send("It looks like you might need help with " + arguments);
     } else {
-        receivedMessage.channel.send("I'm not sure what you need help with. Try `!help [topic]`")
+        receivedMessage.channel.send("I'm not sure what you need help with. Try `!help [topic]`");
     }
 }
 
 function multiplyCommand(arguments, receivedMessage) {
     if (arguments.length < 2) {
-        receivedMessage.channel.send("Not enough values to multiply. Try `!multiply 2 4 10` or `!multiply 5.2 7`")
-        return
+        receivedMessage.channel.send("Not enough values to multiply. Try `!multiply 2 4 10` or `!multiply 5.2 7`");
+        return;
     }
-    let product = 1 
+    let product = 1;
     arguments.forEach((value) => {
-        product = product * parseFloat(value)
-    })
-    receivedMessage.channel.send("The product of " + arguments + " multiplied together is: " + product.toString())
+        product = product * parseFloat(value);
+    });
+    receivedMessage.channel.send("The product of " + arguments + " multiplied together is: " + product.toString());
+}
+
+function punchCommand(arguements, receivedMessage) {
+    let member = receivedMessage.mentions.members.first();
+    console.log('Member is: ' + member.id);
+    if (!member) {
+        receivedMessage.channel.send('I need someone to hit first!');
+    } else if (member.id === config.owner || member.id === client.user.id) {
+        receivedMessage.channel.send('The maid hurt itself in confusion');
+    } else {
+        receivedMessage.channel.send(':right_facing_fist: ' + member);
+    }
 }
 
 
 // Get your bot's secret token from:
 // https://discordapp.com/developers/applications/
 // Click on your application -> Bot -> Token -> "Click to Reveal Token"
-// Create file call Properties containing the following:
-// secret_key: xxxxxxxxx
+// Create file call config.json containing the following:
+// { "secret_key": "xxxxxxxxx", "prefix": "!" }
 // Replace xxxxxxxxx with the secret key
 
-client.login(bot_secret_token)
+client.login(config.secret_key);
